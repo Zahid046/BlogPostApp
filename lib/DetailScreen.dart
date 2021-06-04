80f0c4b4a5e5de3752d4a8b41a7f720d83350180
Zahid Mahmud Emon <zmemon046@gmail.com>
2021-06-04 16:45:24 +0600

    design and details added

--- lib/DetailScreen.dart
+++ lib/DetailScreen.dart
@@ -12,11 +12,38 @@ class DetailScreen extends StatelessWidget {
     // Use the Todo to create the UI.
     return Scaffold(
       appBar: AppBar(
-        title: Text(post.title),
+        title: Text("Post Details"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Text("good"),
+        padding: const EdgeInsets.all(12.0),
+        child: Container(
+          child: Column(
+            children: [
+              Text(post.title,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25.0)),
+              SizedBox(height: 15),
+              Row(
+                children: [
+                  Text(post.userId.toString(),
+                      style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0)),
+                ],
+              ),
+              SizedBox(height: 25),
+              Container(
+                child: Text(
+                  post.body,
+                  style: TextStyle(fontSize: 16.0),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
